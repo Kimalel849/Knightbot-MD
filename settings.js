78db8216c44f3757bba239b537fb91995c018caa
Kimalel849 <giveawayke443@gmail.com>
2026-03-09 23:49:27 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'COLLO', // Your name
-  ownerNumber: '254796774967', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'RYAN', // Your name
+  ownerNumber: '254117002981', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "Private",
   maxStoreMessages: 20, 
