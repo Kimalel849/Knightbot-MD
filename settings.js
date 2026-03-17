30b8183b41580fdc6f309734159fae4c338929c1
Kimalel849 <giveawayke443@gmail.com>
2026-03-17 14:57:49 +0300

    Update bot owner and owner number in settings

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'BRIAN', // Your name
-  ownerNumber: '254117002981', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'KIMALEL', // Your name
+  ownerNumber: '254718681463', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "Private",
   maxStoreMessages: 20, 
